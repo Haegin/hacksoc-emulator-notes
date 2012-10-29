8a2c68b4514f1db37cb6cdbda7e0f296f549ec34
Michael Walker <mike@barrucadu.co.uk>
2012-10-29 17:17:51 +0000

    Merge cpu1 and cpu2 examples

--- lecture1/cpu1.py
+++ lecture1/cpu1.py
@@ -2,4 +2,9 @@ class CPU:
     """A class implementing a PicoBlaze CPU.
     """
 
-    pass
+    def __init__(self):
+        """Set up the initial state of the processor.
+        """
+
+        self.flags = {}
+        self.registers = []
